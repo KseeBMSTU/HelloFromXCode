e42536e3dad00be2db7edc643e17f29112e25204
Ksenia Varaksina <ksiee@openvpn-client.vpn.bmstu.cloud>
2020-05-14 17:34:02 +0300

    Initial Commit

---
+++ HelloFromXCode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloFromXCode
+//
+//  Created by Ksenia Varaksina on 14.05.2020.
+//  Copyright © 2020 Ksenia Varaksina. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
