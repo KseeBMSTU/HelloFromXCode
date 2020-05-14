21c3140675868aa0f14d4289af709320302edb03
Ksenia Varaksina <ksiee@openvpn-client.vpn.bmstu.cloud>
2020-05-14 17:41:01 +0300

    CLion

---
+++ HelloFromXCode/ExtraSource.cpp
@@ -0,0 +1,9 @@
+//
+//  ExtraSource.cpp
+//  HelloFromXCode
+//
+//  Created by Ksenia Varaksina on 14.05.2020.
+//  Copyright © 2020 Ksenia Varaksina. All rights reserved.
+//
+
+#include "ExtraSource.hpp"
---
+++ HelloFromXCode/ExtraSource.hpp
@@ -0,0 +1,14 @@
+//
+//  ExtraSource.hpp
+//  HelloFromXCode
+//
+//  Created by Ksenia Varaksina on 14.05.2020.
+//  Copyright © 2020 Ksenia Varaksina. All rights reserved.
+//
+
+#ifndef ExtraSource_hpp
+#define ExtraSource_hpp
+
+#include <stdio.h>
+
+#endif /* ExtraSource_hpp */
